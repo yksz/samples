9b2f17f8b5539c3c3b4d8186515b98f2ff580774
yksz <user4git@gmail.com>
2014-05-22 01:20:55 +0900

    Modify uploader.go

--- go/server/uploader.go
+++ go/server/uploader.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 const maxMemory = 10000000 // 10MB
@@ -20,41 +22,65 @@ func init() {
 }
 
 func uploadHandler(w http.ResponseWriter, req *http.Request) {
+	defer func() {
+		if r := recover(); r != nil {
+			msg := fmt.Sprintf("%v", r)
+			log.Print(msg)
+			http.Error(w, msg, http.StatusInternalServerError)
+		}
+	}()
 	err := req.ParseMultipartForm(maxMemory)
-	if err != nil {
-		log.Print(err)
-		return
-	}
+	check(err)
 	for key, _ := range req.MultipartForm.File {
 		file, fileHeader, err := req.FormFile(key)
-		if err != nil {
-			log.Print(err)
-			return
-		}
+		check(err)
 		defer file.Close()
-		saveFile(file, fileHeader.Filename)
+		checkFilename(fileHeader.Filename)
+		year, month, day := time.Now().Date()
+		dir := fmt.Sprintf("%d-%d-%d", year, int(month), day)
+		err = saveFile(file, dir+"/"+fileHeader.Filename)
+		check(err)
 	}
 }
 
-func saveFile(r io.Reader, path string) {
-	if path == "" {
-		return
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
-	if strings.HasPrefix(path, "/") {
-		path = path[1:]
+}
+
+func checkFilename(filename string) {
+	if filename == "" {
+		panic(fmt.Sprintf("filename not found: %s", filename))
 	}
+	if strings.Index(filename, "..") != -1 { // security
+		panic(fmt.Sprintf("filename is invalid: %s", filename))
+	}
+}
+
+func saveFile(r io.Reader, path string) error {
 	index := strings.LastIndex(path, "/")
 	if index != -1 {
 		dir := path[:index]
-		os.MkdirAll(dir, os.ModeDir)
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			return err
+		}
 	}
-	bytes, _ := ioutil.ReadAll(r)
-	ioutil.WriteFile(path, bytes, 0644)
+	bytes, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(path, bytes, 0644)
+	if err != nil {
+		return err
+	}
 	log.Printf("save a file: %s", path)
+	return nil
 }
 
 func main() {
-	log.Print("the upload server started: port=" + port)
+	log.Print("the upload server is listening on port " + port)
 	http.HandleFunc("/", uploadHandler)
 	http.ListenAndServe(":"+port, nil)
 }
