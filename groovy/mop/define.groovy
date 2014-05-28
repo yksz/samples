85ebc3f83b302f414b19935e01f5c7a5efcb8418
yksz <user4git@gmail.com>
2014-05-28 10:18:18 +0900

    Modify mop/define.groovy

--- groovy/mop/define.groovy
+++ groovy/mop/define.groovy
@@ -1,7 +1,23 @@
-Integer.metaClass.define {
-    reverse = {
-        return delegate.toString().reverse() as Integer
-    }
-}
+class Empty { String name }
+Empty.metaClass.constructor << { String arg -> println "constructor: $arg"; new Empty(name:arg) }
+Empty.metaClass.static.myMethod << { args -> "static myMethod" }
+Empty.metaClass.myMethod << { args -> "myMethod" }
 
-println 123456.reverse()
+def obj1 = new Empty("obj1")
+assert obj1.myMethod() == "myMethod"
+assert Empty.myMethod() == "static myMethod"
+
+Empty.metaClass.constructor = { String arg -> println "replaced constructor: $arg"; new Empty(name:arg) }
+def obj2 = new Empty("obj2")
+obj2.metaClass {
+    myProperty = "myProperty"
+    myMethod = { args -> "replaced myMethod" }
+}
+assert obj2.myProperty == "myProperty"
+assert obj2.myMethod() == "replaced myMethod"
+
+
+Integer.metaClass.define {
+    reverse { return delegate.toString().reverse() as Integer }
+}
+assert 123456.reverse() == 654321
