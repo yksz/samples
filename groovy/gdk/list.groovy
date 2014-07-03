fa8181d3ab8732668a9e03aeb49f1791286e5b47
yksz <user4git@gmail.com>
2014-07-03 11:15:32 +0900

    Modify groovy/gdk samples

--- groovy/gdk/list.groovy
+++ groovy/gdk/list.groovy
@@ -1,7 +1,15 @@
 assert [] instanceof ArrayList == true
 
-// 1. collection
-//  - operator
+// 1. Collection or Iterable
+// - loop
+x = 1
+for (i in [1,2,3]) assert i == x++
+x = 1
+[1,2,3].each { assert it == x++ }
+x = 3
+[1,2,3].reverseEach { assert it == x-- }
+
+// - operator
 assert [1,2,3] + 4    == [1,2,3,4]
 assert [1,2,3] + [4]  == [1,2,3,4]
 assert [1,2,3] - 3    == [1, 2]
@@ -11,7 +19,7 @@ assert [1,2,3] << 4   == [1,2,3,4]
 assert [1,2,3] << [4] == [1,2,3,[4]]
 assert [1,*[2,3]]     == [1,2,3]
 
-//  - util
+// - util
 assert [1,3,3].count(3)  == 2
 assert [1,2,3].join(",") == "1,2,3"
 assert [1,2,3].max()     == 3
@@ -20,7 +28,7 @@ assert [1,2,3].sum()     == 6
 assert [3,2,1].sort()    == [1,2,3]
 assert [1,3,3].unique()  == [1,3]
 
-//  - closure
+// - closure
 assert [1,2,3].any { it > 2 }              == true  // or
 assert [1,2,3].every { it > 2 }            == false // and
 assert [1,2,3].collect { it + 1 }          == [2,3,4]
@@ -30,18 +38,18 @@ assert [1,2,3].groupBy { it % 2 }          == [1:[1,3], 0:[2]]
 assert [1,2,3].inject { acc, v -> acc + v} == (((0+1)+2)+3)
 assert [1,2,3].split { it % 2 == 0 }       == [[2],[1,3]]
 
-//  - regexp
+// - regexp
 assert ["ab","ac","bc"].find { it =~ /a+/ }    == "ab"
 assert ["ab","ac","bc"].findAll { it =~ /a+/ } == ["ab","ac"]
 assert ["ab","ac","bc"].grep(~/a.+/ )          == ["ab","ac"]
 
-//  - other
+// - other
 assert [1,2,3].disjoint([3,4,5])    == false
 assert [1,2,3].disjoint([4,5,6])    == true
 assert [1,2,3,4].intersect([3,4,5]) == [3,4]
 assert [1,[2,3]].flatten()          == [1,2,3]
 
-// 2. list
+// 2. List
 assert [1,2,3].first() == 1
 assert [1,2,3].last()  == 3
 assert [1,2,3].head()  == 1
--- groovy/gdk/map.groovy
+++ groovy/gdk/map.groovy
@@ -1,12 +1,22 @@
-assert [:] instanceof HashMap == true
+assert [:] instanceof LinkedHashMap == true
 
-//  - operator
+// - loop
+x = 1
+for (e in [a:1,b:2,c:3]) assert e.value == x++
+x = 1
+for (v in [a:1,b:2,c:3].values()) assert v == x++
+x = 1
+[a:1,b:2,c:3].each { k,v -> assert v == x++ }
+x = 3
+[a:1,b:2,c:3].reverseEach { k,v -> assert v == x-- }
+
+// - operator
 assert [a:1,b:2] + [c:3]  == [a:1,b:2,c:3]
 assert [a:1,b:2] - [b:2]  == [a:1]
 assert [a:1,b:2] << [c:3] == [a:1,b:2,c:3]
 assert [a:1,*:[b:2,c:3]]  == [a:1,b:2,c:3]
 
-//  - closure
+// - closure
 assert [a:1,b:2].any { it.value > 1 }                    == true  // or
 assert [a:1,b:2].every { it.value > 1 }                  == false // and
 assert [a:1,b:2].collect { k,v -> k * v }                == ["a","bb"]
@@ -19,12 +29,19 @@ assert [a:1,b:2,c:3].findAll { it.value % 2 != 0 }       == [a:1,c:3]
 assert [a:1,b:2].groupBy { it.value % 2 }                == [0:[b:2],1:[a:1]]
 assert [a:1,b:2].groupEntriesBy { it.value % 2 }[0]*.key == ["b"]
 assert [a:1,b:2].inject([]) { list, k, v -> list + [k] } == ["a","b"]
+assert [a:1,b:2,c:3].max { it.value }.key                == "c"
+assert [a:1,b:2,c:3].min { it.value }.key                == "a"
+assert [a:2,b:3,c:1].sort { a,b -> a.value <=> b.value } == [c:1,a:2,b:3]
 
-//  - other
+// - other
 assert [a:1,b:2,c:3].drop(1) == [b:2,c:3]
 assert [a:1,b:2,c:3].drop(3) == [:]
 assert [a:1,b:2].get("c", 3) == 3
 assert [a:1,b:2,c:3].intersect([b:2,c:3,d:4]) == [b:2,c:3]
+assert [b:1,c:2,a:3].sort()*.value == [3,1,2]
+assert [a:1,b:2,c:3].subMap(["a","c"]) == [a:1,c:3]
+assert [a:1,b:2,c:3].take(1) == [a:1]
+assert [a:1,b:2,c:3].take(3) == [a:1,b:2,c:3]
 
 def map = [a:1,b:2,c:3]
 assert map.a == 1
