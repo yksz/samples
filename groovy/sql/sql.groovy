e05391ea6dbb512b5f7fe122c649ce96a65f00a3
yksz <user4git@gmail.com>
2015-01-08 12:51:49 +0900

    Modify groovy/sql to read a external file for db

--- groovy/sql/sql.groovy
+++ groovy/sql/sql.groovy
@@ -1,27 +1,27 @@
 @Grab(group='com.h2database', module='h2', version='1.3.+')
 import groovy.sql.Sql
 
-def db = [
-    url:'jdbc:h2:mem:testDB;MVCC=TRUE;LOCK_TIMEOUT=10000',
-    user:'sa',
-    password:'',
-    driver:'org.h2.Driver'
-]
-def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
+def config = 'h2.properties'
+if (args.length > 0)
+    config = args[0]
+println "$config"
 
+def db = new Properties()
+new File(config).withInputStream {
+    db.load(it)
+}
+
+def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
 sql.execute '''
     create table PROJECT (
         id integer not null,
         name varchar(50),
-        url varchar(100),
+        url varchar(100)
     )
 '''
-
 def params = [10, 'Groovy', 'http://groovy.codehaus.org']
 sql.execute 'insert into PROJECT (id, name, url) values (?, ?, ?)', params
-
 sql.eachRow('select * from PROJECT') { row ->
     println "${row.name.padRight(10)} ($row.url)"
 }
-
 sql.close()
