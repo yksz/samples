0f58fd2dae0c94660647e7e728a23632b7a931a8
yksz <user4git@gmail.com>
2015-03-23 14:14:06 +0900

    Add c/error/goto.c

---
+++ c/error/goto.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void copy(FILE* src, FILE* dst)
+{
+    int c;
+    while ((c = getc(src)) != EOF) {
+        putc(c, dst);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc <= 2) {
+        fprintf(stderr, "usage: ./a.out <src> <dst>\n");
+        exit(EXIT_FAILURE);
+    }
+    char* src_name = argv[1];
+    char* dst_name = argv[2];
+
+    int result = EXIT_FAILURE;
+    FILE *src, *dst;
+    if ((src = fopen(src_name, "r")) == NULL) {
+        goto SRC_ERROR;
+    }
+    if ((dst = fopen(dst_name, "w")) == NULL) {
+        goto DST_ERROR;
+    }
+
+    copy(src, dst);
+    result = EXIT_SUCCESS;
+
+    fclose(dst);
+DST_ERROR:
+    fclose(src);
+SRC_ERROR:
+    return result;
+}
