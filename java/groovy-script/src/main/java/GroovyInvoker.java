18644678a258a3a09b3eba99d40cef218a168726
yksz <user4git@gmail.com>
2015-04-30 14:33:54 +0900

    Update java/groovy-script

--- java/groovy-script/src/main/java/GroovyInvoker.java
+++ java/groovy-script/src/main/java/GroovyInvoker.java
@@ -1,11 +1,35 @@
-import java.io.File;
 import groovy.lang.GroovyShell;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.SimpleBindings;
 
 public class GroovyInvoker {
 
-    public static void main(String[] args) throws Exception {
+    private static void invokeScriptEngine(File file, String[] args) throws Exception {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("groovy");
+        Bindings bindings = new SimpleBindings();
+        bindings.put("args", args);
+        try (Reader reader = new FileReader(file)) {
+            engine.eval(reader, bindings);
+        }
+    }
+
+    private static void invokeGroovyShell(File file, String[] args) throws Exception {
         GroovyShell groovy = new GroovyShell();
-        groovy.run(new File("src/main/groovy/hello.groovy"), args);
+        groovy.run(file, args);
+    }
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("src/main/groovy/hello.groovy");
+        invokeScriptEngine(file, args);
+        invokeGroovyShell(file, args);
     }
 
 }
