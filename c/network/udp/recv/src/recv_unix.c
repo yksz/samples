a824b3f2b7f634b3a39cd96be409bfd59e17e6a5
yksz <user4git@gmail.com>
2015-09-16 00:48:14 +0900

    Modify c/network/udp/recv for unix

--- c/network/udp/recv/src/recv_unix.c
+++ c/network/udp/recv/src/recv_unix.c
@@ -6,20 +6,25 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-static const int PORT = 8080;
+static const int DEFAULT_PORT = 8080;
 
-static void recv_data(int sockfd)
+static void wait_and_recv(int sockfd)
 {
     char buf[64];
     for (;;) {
         memset(buf, 0, sizeof(buf));
-        recv(sockfd, buf, sizeof(buf), 0);
+        recvfrom(sockfd, buf, sizeof(buf), 0, NULL, NULL);
         printf("%s\n", buf);
     }
 }
 
 int main(int argc, char** argv)
 {
+    int port = DEFAULT_PORT;
+    if (argc > 1) {
+        port = atoi(argv[1]);
+    }
+
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd == -1) {
         perror("socket");
@@ -29,15 +34,15 @@ int main(int argc, char** argv)
     struct sockaddr_in recv_addr;
     memset(&recv_addr, 0, sizeof(recv_addr));
     recv_addr.sin_family = AF_INET;
-    recv_addr.sin_port = htons(PORT);
+    recv_addr.sin_port = htons(port);
     recv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     if (bind(sockfd, (struct sockaddr*) &recv_addr, sizeof(recv_addr)) == -1) {
         perror("bind");
         exit(1);
     }
-    printf("Listening on port %d\n", PORT);
 
-    recv_data(sockfd);
+    printf("Listening on port %d\n", port);
+    wait_and_recv(sockfd);
 
     close(sockfd);
     return 0;
