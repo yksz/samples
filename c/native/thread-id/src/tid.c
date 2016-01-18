3cb9f0b5477b22b5f3dab55accf76c65754ec3f6
yksz <user4git@gmail.com>
2016-01-18 15:36:22 +0900

    Add c/native/thread-id

---
+++ c/native/thread-id/src/tid.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#if (defined(_WIN32) || defined(_WIN64)) || defined(__CYGWIN__)
+#include <windows.h>
+#else
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#endif
+
+static long getCurrentThreadID()
+{
+#if defined(__linux)
+    return syscall(SYS_gettid);
+#elif defined(__APPLE) && defined(__MACH__)
+    return syscall(SYS_thread_selfid);
+#elif (defined(_WIN32) || defined(_WIN64)) || defined(__CYGWIN__)
+    return GetCurrentThreadId();
+#else
+    return -1;
+#endif
+}
+
+int main(void)
+{
+    printf("ThreadID=%ld\n", getCurrentThreadID());
+    return 0;
+}
