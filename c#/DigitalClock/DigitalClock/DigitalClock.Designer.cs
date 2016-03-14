b6fc99c6384fad392ee14454fbe023f3cded779e
yksz <user4git@gmail.com>
2016-03-14 17:32:09 +0900

    c#: Add DigitalClock

---
+++ c#/DigitalClock/DigitalClock/DigitalClock.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DigitalClock
+{
+    partial class DigitalClock
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Consolas", 48F);
+            this.timeLabel.Location = new System.Drawing.Point(15, 4);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 75);
+            this.timeLabel.TabIndex = 0;
+            // 
+            // DigitalClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 82);
+            this.Controls.Add(this.timeLabel);
+            this.Name = "DigitalClock";
+            this.Text = "DigitalClock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ c#/DigitalClock/DigitalClock/DigitalClock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class DigitalClock : Form
+    {
+        const int Interval = 1000;
+        System.Windows.Forms.Timer timer;
+
+        public DigitalClock()
+        {
+            InitializeComponent();
+
+            UpdateTimeLabel();
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = Interval;
+            timer.Tick += (sender, e) =>
+            {
+                UpdateTimeLabel();
+            };
+            timer.Start();
+        }
+
+        void UpdateTimeLabel()
+        {
+            DateTime time = DateTime.Now;
+            timeLabel.Text = time.ToString("HH:mm.ss");
+        }
+    }
+}
