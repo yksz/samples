c7a3d10b6a75da48552311185c0c1461ac9573e8
yksz <user4git@gmail.com>
2016-03-30 19:34:22 +0900

    c: Modify native/mac-address

--- c/native/mac-address/src/mac_unix.c
+++ c/native/mac-address/src/mac_unix.c
@@ -5,31 +5,78 @@
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 
-static void getMACAddress(unsigned char* addr, size_t len)
+static int getMACAddressByName(char* name, unsigned char* addr, size_t len)
 {
     int fd;
-    struct ifreq ifreq;
-    size_t i;
+    struct ifreq ifr;
 
     fd = socket(AF_INET, SOCK_DGRAM, 0);
-    ifreq.ifr_addr.sa_family = AF_INET;
-    strncpy(ifreq.ifr_name, "eth0", IFNAMSIZ - 1);
-    ioctl(fd, SIOCGIFHWADDR, &ifreq);
+    if (fd == -1) {
+        return 0;
+    }
+    ifr.ifr_addr.sa_family = AF_INET;
+    strncpy(ifr.ifr_name, name, IFNAMSIZ - 1);
+    if (ioctl(fd, SIOCGIFHWADDR, &ifr) == -1) {
+        close(fd);
+        return 0;
+    }
     close(fd);
 
-    for (i = 0; i < len; i++) {
-        addr[i] = (unsigned char) ifreq.ifr_hwaddr.sa_data[i];
-    }
+    memcpy(addr, ifr.ifr_hwaddr.sa_data, len);
+    return 1;
 }
 
-int main(void)
+
+static int getMACAddressOfFirstInterface(unsigned char* addr, size_t len)
 {
-    unsigned char macAddr[6];
-    int i = 0;
+    int fd;
+    struct ifreq ifr[8]; /* max number of interfaces */
+    struct ifconf ifc;
+    int nifs;
+    int i;
+    int isfirst = 1;
 
-    getMACAddress(macAddr, sizeof(macAddr));
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd == -1) {
+        return 0;
+    }
+    ifc.ifc_len = sizeof(ifr);
+    ifc.ifc_ifcu.ifcu_buf = (void*) ifr;
+    if (ioctl(fd, SIOCGIFCONF, &ifc) == -1) {
+        close(fd);
+        return 0;
+    }
+    nifs = ifc.ifc_len / sizeof(struct ifreq);
+    for (i = 0; i < nifs; i++) {
+        if (ioctl(fd, SIOCGIFFLAGS, &ifr[i]) != -1
+                && ifr[i].ifr_flags & IFF_LOOPBACK) { /* loopback */
+            continue;
+        }
+        if (ioctl(fd, SIOCGIFHWADDR, &ifr[i]) != -1) {
+            memcpy(addr, ifr[i].ifr_hwaddr.sa_data, len);
+            close(fd);
+            return 1;
+        }
+    }
+    close(fd);
+    return 0;
+}
 
-    printf("%.2x", macAddr[i]);
+int main(int argc, char* argv[])
+{
+    char* ifname = "";
+    unsigned char macAddr[6] = {0};
+    int i;
+
+    if (argc > 1) {
+        ifname = argv[1];
+    }
+
+    if (!getMACAddressByName(ifname, macAddr, sizeof(macAddr))) {
+        getMACAddressOfFirstInterface(macAddr, sizeof(macAddr));
+    }
+
+    printf("%.2x", macAddr[0]);
     for (i = 1; i < sizeof(macAddr); i++) {
         printf(":%.2x", macAddr[i]);
     }
--- c/native/mac-address/src/mac_windows.c
+++ c/native/mac-address/src/mac_windows.c
@@ -1,49 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <winsock2.h>
 #include <iphlpapi.h>
 
 #pragma comment(lib, "iphlpapi.lib")
 
-static void getMACAddress(unsigned char* addr, size_t len)
+static int getMACAddressByName(char* name, unsigned char* addr, size_t len)
 {
     PIP_ADAPTER_INFO pAdapterInfo;
     PIP_ADAPTER_INFO pAdapter;
     ULONG ulOutBufLen = sizeof(IP_ADAPTER_INFO);
-    size_t i;
 
     pAdapterInfo = (IP_ADAPTER_INFO*) malloc(sizeof(IP_ADAPTER_INFO));
     if (pAdapterInfo == NULL) {
         fprintf(stderr, "ERROR: out of memory\n");
-        return;
+        return 0;
     }
     if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == ERROR_BUFFER_OVERFLOW) {
         free(pAdapterInfo);
         pAdapterInfo = (IP_ADAPTER_INFO*) malloc(ulOutBufLen);
         if (pAdapterInfo == NULL) {
             fprintf(stderr, "ERROR: out of memory\n");
-            return;
+            return 0;
         }
     }
     if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == NO_ERROR) {
-        pAdapter = pAdapterInfo;
-        if (pAdapter) {
-            for (i = 0; i < len; i++) {
-                addr[i] = pAdapter->Address[i];
+        for (pAdapter = pAdapterInfo; pAdapter != NULL; pAdapter = pAdapter->Next) {
+            if (strcmp(pAdapter->AdapterName, name) == 0) {
+                memcpy(addr, pAdapter->Address, len);
+                free(pAdapterInfo);
+                return 1;
             }
         }
     }
     free(pAdapterInfo);
+    return 0;
 }
 
-int main(void)
+static int getMACAddressOfFirstInterface(unsigned char* addr, size_t len)
 {
-    unsigned char macAddr[6];
-    int i = 0;
+    PIP_ADAPTER_INFO pAdapterInfo;
+    PIP_ADAPTER_INFO pAdapter;
+    ULONG ulOutBufLen = sizeof(IP_ADAPTER_INFO);
 
-    getMACAddress(macAddr, sizeof(macAddr));
+    pAdapterInfo = (IP_ADAPTER_INFO*) malloc(sizeof(IP_ADAPTER_INFO));
+    if (pAdapterInfo == NULL) {
+        fprintf(stderr, "ERROR: out of memory\n");
+        return 0;
+    }
+    if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == ERROR_BUFFER_OVERFLOW) {
+        free(pAdapterInfo);
+        pAdapterInfo = (IP_ADAPTER_INFO*) malloc(ulOutBufLen);
+        if (pAdapterInfo == NULL) {
+            fprintf(stderr, "ERROR: out of memory\n");
+            return 0;
+        }
+    }
+    if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == NO_ERROR) {
+        for (pAdapter = pAdapterInfo; pAdapter != NULL; pAdapter = pAdapter->Next) {
+            if (pAdapter->Type != MIB_IF_TYPE_LOOPBACK) { /* not loopback */
+                memcpy(addr, pAdapter->Address, len);
+                free(pAdapterInfo);
+                return 1;
+            }
+        }
+    }
+    free(pAdapterInfo);
+    return 0;
+}
 
-    printf("%.2x", macAddr[i]);
+int main(int argc, char* argv[])
+{
+    char* ifname = "";
+    unsigned char macAddr[6] = {0};
+    int i;
+
+    if (argc > 1) {
+        ifname = argv[1];
+    }
+
+    if (!getMACAddressByName(ifname, macAddr, sizeof(macAddr))) {
+        getMACAddressOfFirstInterface(macAddr, sizeof(macAddr));
+    }
+
+    printf("%.2x", macAddr[0]);
     for (i = 1; i < sizeof(macAddr); i++) {
         printf(":%.2x", macAddr[i]);
     }
